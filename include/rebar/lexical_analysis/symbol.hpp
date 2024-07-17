03ab83a0e74b32785698a05d6ff694f485049ad5
Max Goddard <max@altis.gg>
2024-07-16 23:53:33 -0700

    Added non-special character flag to symbol map to avoid parsing symbols when they should be a part of an identifier.

--- include/rebar/lexical_analysis/symbol.hpp
+++ include/rebar/lexical_analysis/symbol.hpp
@@ -15,48 +15,56 @@ namespace rebar {
      * keywords and other reserved sequences.
      */
     enum class symbol {
-        tilda, // ~
-        exclamation, // !
-        exclamation_equals, // !=
-        at, // @
-        pound, // #
-        dollar, // $
-        percent, // %
-        carrot, // ^
-        carrot_equals, // ^=
-        ampersand, // &
-        ampersand_equals, // &=
-        double_ampersand, // &&
+        tilda,                   // ~
+        exclamation,             // !
+        exclamation_equals,      // !=
+        at,                      // @
+        pound,                   // #
+        dollar,                  // $
+        percent,                 // %
+        carrot,                  // ^
+        carrot_equals,           // ^=
+        ampersand,               // &
+        ampersand_equals,        // &=
+        double_ampersand,        // &&
         double_ampersand_equals, // &&=
-        star, // *
-        star_equals, // *=
-        parenthesis_left, // (
-        paranthesis_right, // )
-        bracket_left, // [
-        bracket_right, // ]
-        brace_left, // {
-        brace_right, // }
-        minus, // -
-        minus_equals, // -=
-        plus, // +
-        plus_equals, // +=
-        equals, // =
-        double_equals, // ==
-        slash, // /
-        slash_equals, // /=
-        colon, // :
-        semicolon, // ;
-        comma, // ,
-        carrot_left, // <
-        carrot_right, // >
-        pipe, // |
-        pipe_equals, // |=
-        double_pipe, // ||
-        double_pipe_equals, // ||=
+        star,                    // *
+        star_equals,             // *=
+        parenthesis_left,        // (
+        paranthesis_right,       // )
+        bracket_left,            // [
+        bracket_right,           // ]
+        brace_left,              // {
+        brace_right,             // }
+        minus,                   // -
+        minus_equals,            // -=
+        plus,                    // +
+        plus_equals,             // +=
+        equals,                  // =
+        double_equals,           // ==
+        slash,                   // /
+        slash_equals,            // /=
+        colon,                   // :
+        semicolon,               // ;
+        comma,                   // ,
+        carrot_left,             // <
+        carrot_right,            // >
+        pipe,                    // |
+        pipe_equals,             // |=
+        double_pipe,             // ||
+        double_pipe_equals,      // ||=
+        boolean_true,            // true
+        boolean_false,           // false
     };
 
-    /// A map of plaintext symbols to enumeration values.
-    using symbol_map = std::unordered_map<std::string_view, symbol>;
+    /**
+     * A map of plaintext symbols to enumeration values.
+     * Enumeration values are paired with a boolean that marks whether the
+     * symbol is a non-special character symbol (if true and the symbol is
+     * found, but begins an identifier, the symbol will not be parsed, and
+     * will be included in the identifier).
+     */
+    using symbol_map = std::unordered_map<std::string_view, std::pair<symbol, bool>>;
 
     /**
      * Generate the default Rebar symbol map.
@@ -64,44 +72,46 @@ namespace rebar {
      */
     inline symbol_map default_symbol_map() {
         return {
-            { "~",   symbol::tilda,                   },
-            { "!",   symbol::exclamation,             },
-            { "!=",  symbol::exclamation_equals,      },
-            { "@",   symbol::at,                      },
-            { "#",   symbol::pound,                   },
-            { "$",   symbol::dollar,                  },
-            { "%",   symbol::percent,                 },
-            { "^",   symbol::carrot,                  },
-            { "^=",  symbol::carrot_equals,           },
-            { "&",   symbol::ampersand,               },
-            { "&=",  symbol::ampersand_equals,        },
-            { "&&",  symbol::double_ampersand,        },
-            { "&&=", symbol::double_ampersand_equals, },
-            { "*",   symbol::star,                    },
-            { "*=",  symbol::star_equals,             },
-            { "(",   symbol::parenthesis_left,        },
-            { ")",   symbol::paranthesis_right,       },
-            { "[",   symbol::bracket_left,            },
-            { "]",   symbol::bracket_right,           },
-            { "{",   symbol::brace_left,              },
-            { "}",   symbol::brace_right,             },
-            { "-",   symbol::minus,                   },
-            { "-=",  symbol::minus_equals,            },
-            { "+",   symbol::plus,                    },
-            { "+=",  symbol::plus_equals,             },
-            { "=",   symbol::equals,                  },
-            { "==",  symbol::double_equals,           },
-            { "/",   symbol::slash,                   },
-            { "/=",  symbol::slash_equals,            },
-            { ":",   symbol::colon,                   },
-            { ";",   symbol::semicolon,               },
-            { ",",   symbol::comma,                   },
-            { "<",   symbol::carrot_left,             },
-            { ">",   symbol::carrot_right,            },
-            { "|",   symbol::pipe,                    },
-            { "|=",  symbol::pipe_equals,             },
-            { "||",  symbol::double_pipe,             },
-            { "||=", symbol::double_pipe_equals,      },
+            { "~",     { symbol::tilda,                   false, } },
+            { "!",     { symbol::exclamation,             false, } },
+            { "!=",    { symbol::exclamation_equals,      false, } },
+            { "@",     { symbol::at,                      false, } },
+            { "#",     { symbol::pound,                   false, } },
+            { "$",     { symbol::dollar,                  false, } },
+            { "%",     { symbol::percent,                 false, } },
+            { "^",     { symbol::carrot,                  false, } },
+            { "^=",    { symbol::carrot_equals,           false, } },
+            { "&",     { symbol::ampersand,               false, } },
+            { "&=",    { symbol::ampersand_equals,        false, } },
+            { "&&",    { symbol::double_ampersand,        false, } },
+            { "&&=",   { symbol::double_ampersand_equals, false, } },
+            { "*",     { symbol::star,                    false, } },
+            { "*=",    { symbol::star_equals,             false, } },
+            { "(",     { symbol::parenthesis_left,        false, } },
+            { ")",     { symbol::paranthesis_right,       false, } },
+            { "[",     { symbol::bracket_left,            false, } },
+            { "]",     { symbol::bracket_right,           false, } },
+            { "{",     { symbol::brace_left,              false, } },
+            { "}",     { symbol::brace_right,             false, } },
+            { "-",     { symbol::minus,                   false, } },
+            { "-=",    { symbol::minus_equals,            false, } },
+            { "+",     { symbol::plus,                    false, } },
+            { "+=",    { symbol::plus_equals,             false, } },
+            { "=",     { symbol::equals,                  false, } },
+            { "==",    { symbol::double_equals,           false, } },
+            { "/",     { symbol::slash,                   false, } },
+            { "/=",    { symbol::slash_equals,            false, } },
+            { ":",     { symbol::colon,                   false, } },
+            { ";",     { symbol::semicolon,               false, } },
+            { ",",     { symbol::comma,                   false, } },
+            { "<",     { symbol::carrot_left,             false, } },
+            { ">",     { symbol::carrot_right,            false, } },
+            { "|",     { symbol::pipe,                    false, } },
+            { "|=",    { symbol::pipe_equals,             false, } },
+            { "||",    { symbol::double_pipe,             false, } },
+            { "||=",   { symbol::double_pipe_equals,      false, } },
+            { "true",  { symbol::boolean_true,            true,  } },
+            { "false", { symbol::boolean_false,           true,  } },
         };
     }
 
