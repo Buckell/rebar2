7fe4202cac2fe361748c2141b8c83af29afa2f10
Buckell <buckell@buckell.dev>
2024-07-23 13:44:20 -0700

    Implemented operation tree class to store the results of semantic analysis. Created class shells for other constructs.

---
+++ include/rebar/semantic_analysis/semantic_unit.hpp
@@ -0,0 +1,19 @@
+//
+// Created by maxng on 23/07/2024.
+//
+
+#ifndef SEMANTIC_UNIT_H
+#define SEMANTIC_UNIT_H
+
+#include <rebar/semantic_analysis/operation_tree.hpp>
+
+namespace rebar {
+
+    class semantic_unit {
+        operation_tree m_base_scope;
+
+    };
+
+}
+
+#endif //SEMANTIC_UNIT_H
