a9200d236e56a6a453a1724fd0b3854f7baaa0d9
Buckell <buckell@buckell.dev>
2024-07-26 13:02:51 -0700

    Added semantic unit to handle semantic analysis results and other information.

--- include/rebar/semantic_analysis/semantic_unit.hpp
+++ include/rebar/semantic_analysis/semantic_unit.hpp
@@ -12,7 +12,36 @@ namespace rebar {
     class semantic_unit {
         operation_tree m_base_scope;
 
+    public:
+        semantic_unit() noexcept = default;
+
+        semantic_unit(semantic_unit const &) noexcept = default;
+        semantic_unit(semantic_unit &&)      noexcept = default;
+
+        semantic_unit & operator = (semantic_unit const &) noexcept = default;
+        semantic_unit & operator = (semantic_unit &&)      noexcept = default;
+
+        inline void apply_base_scope(operation_tree a_operation_tree) noexcept;
+
+        inline operation_tree & base_scope() noexcept;
+
+        [[nodiscard]]
+        inline operation_tree const & base_scope() const noexcept;
     };
+
+    // ###################################### INLINE DEFINITIONS ######################################
+
+    void semantic_unit::apply_base_scope(operation_tree a_operation_tree) noexcept {
+        m_base_scope = std::move(a_operation_tree);
+    }
+
+    operation_tree & semantic_unit::base_scope() noexcept {
+        return m_base_scope;
+    }
+
+    operation_tree const & semantic_unit::base_scope() const noexcept {
+        return m_base_scope;
+    }
 
 }
 
