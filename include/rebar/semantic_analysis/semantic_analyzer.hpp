d35f9bfae2418ecaa4379f4ee33bbec7e211f4d6
Buckell <buckell@buckell.dev>
2024-07-26 13:28:37 -0700

    Implemented semantic analyzer. Expression parsing handles precedence, simple binary operators of both associations and simple unary operators of both associations.

--- include/rebar/semantic_analysis/semantic_analyzer.hpp
+++ include/rebar/semantic_analysis/semantic_analyzer.hpp
@@ -5,6 +5,10 @@
 #ifndef SEMANTIC_ANALYZER_H
 #define SEMANTIC_ANALYZER_H
 
+#include <rebar/lexical_analysis/lexical_unit.hpp>
+#include <rebar/semantic_analysis/operators.hpp>
+#include <rebar/semantic_analysis/semantic_unit.hpp>
+
 namespace rebar {
 
     /**
@@ -12,8 +16,86 @@ namespace rebar {
      * tree.
      */
     class semantic_analyzer {
+        operator_registry m_operator_registry;
 
+    public:
+        inline explicit semantic_analyzer(operator_registry a_operator_registry = default_operator_registry()) noexcept;
+
+        semantic_analyzer(semantic_analyzer const &) noexcept = default;
+        semantic_analyzer(semantic_analyzer &&)      noexcept = default;
+
+        semantic_analyzer & operator = (semantic_analyzer const &) noexcept = default;
+        semantic_analyzer & operator = (semantic_analyzer&&)       noexcept = default;
+
+        /**
+         * Perform semantic analysis on a group of tokens.
+         * @param a_semantic_unit The semantic unit to use for results and constraints.
+         * @param a_lexical_unit The lexical unit to use as a token input.
+         */
+        void perform_analysis(semantic_unit & a_semantic_unit, lexical_unit const & a_lexical_unit) const;
+
+    private:
+        operation_tree      parse_block_scope(semantic_unit & a_semantic_unit, std::span<token const> a_tokens) const;
+        operation_tree_node parse_expression(semantic_unit & a_semantic_unit, std::span<token const> a_tokens) const;
+
+        /**
+         * Find the next token matching the predicate function on the same
+         * scope level.
+         * @tparam t_predicate Type of predicate function.
+         * @param a_begin Beginning token iterator.
+         * @param a_end Ending token iterator.
+         * @param a_predicate Predicate against which to check.
+         * @return The iterator matched by the predicate or the ending iterator
+         *         if nothing is found.
+         */
+        template <std::predicate<token const &> t_predicate>
+        static std::span<token const>::iterator tokens_scoped_increment_find(
+            std::span<token const>::iterator a_begin,
+            std::span<token const>::iterator a_end,
+            t_predicate                      a_predicate
+        ) noexcept;
     };
+
+    // ###################################### INLINE DEFINITIONS ######################################
+
+    semantic_analyzer::semantic_analyzer(operator_registry a_operator_registry) noexcept :
+        m_operator_registry(std::move(a_operator_registry))
+    {}
+
+    template<std::predicate<token const &> t_predicate>
+    std::span<token const>::iterator semantic_analyzer::tokens_scoped_increment_find(
+        std::span<token const>::iterator const a_begin,
+        std::span<token const>::iterator const a_end,
+        t_predicate a_predicate
+    ) noexcept {
+        auto token_it = a_begin;
+        std::size_t scope_level = 0;
+
+        while (++token_it != a_end) {
+            auto const & current_token = *token_it;
+
+            if (scope_level == 0 && a_predicate(current_token)) {
+                return token_it;
+            }
+
+            if (
+                current_token == symbol::brace_left ||
+                current_token == symbol::bracket_left ||
+                current_token == symbol::parenthesis_left
+            ) {
+                ++scope_level;
+            }
+            else if (
+                current_token == symbol::brace_right ||
+                current_token == symbol::bracket_right ||
+                current_token == symbol::parenthesis_right
+            ) {
+                --scope_level;
+            }
+        }
+
+        return a_end;
+    }
 
 }
 
---
+++ source/semantic_analysis/semantic_analyzer.cpp
@@ -0,0 +1,168 @@
+//
+// Created by maxng on 24/07/2024.
+//
+
+#include <algorithm>
+#include <ranges>
+#include <rebar/semantic_analysis/semantic_analyzer.hpp>
+
+namespace rebar {
+
+    void semantic_analyzer::perform_analysis(semantic_unit & a_semantic_unit, lexical_unit const & a_lexical_unit) const {
+        a_semantic_unit.apply_base_scope(parse_block_scope(a_semantic_unit, a_lexical_unit.tokens()));
+    }
+
+    operation_tree semantic_analyzer::parse_block_scope(semantic_unit & a_semantic_unit, std::span<token const> a_tokens) const {
+        operation_tree base_scope(operation::scope);
+
+        auto token_it = a_tokens.begin();
+
+        while (token_it != a_tokens.end()) {
+            // Testing for non-simple statement constructions.
+
+            // Statement parsing.
+            auto const statement_begin = token_it;
+
+            auto const statement_end = tokens_scoped_increment_find(
+                statement_begin,
+                a_tokens.end(),
+                [](auto const & a_token) {
+                    return a_token == symbol::semicolon;
+                }
+            );
+
+            token_it = statement_end + 1;
+
+            base_scope.push_operand(
+                parse_expression(
+                    a_semantic_unit,
+                    { statement_begin, statement_end }
+                )
+            );
+        }
+
+        return base_scope;
+    }
+
+    operation_tree_node semantic_analyzer::parse_expression(semantic_unit & a_semantic_unit, std::span<token const> const a_tokens) const { // NOLINT(*-no-recursion)
+        if (a_tokens.size() == 1) {
+            return a_tokens[0];
+        }
+
+        auto expression_tree = std::make_unique<operation_tree>();
+
+        auto symbol_tokens = std::ranges::views::filter(a_tokens, &token::is_symbol);
+
+        if (symbol_tokens.empty()) {
+            // TODO: Throw error.
+        }
+
+        // Projection function for token precedence.
+        auto const operator_token_precedence = [this](auto const & current_token) {
+            const auto it = std::ranges::find(
+                m_operator_registry,
+                current_token.get_symbol(),
+                &operator_info::identifier
+            );
+
+            return it != m_operator_registry.cend() ? it->precedence : 0;
+        };
+
+        // Find lowest precedence operator.
+        auto const & lowest_operator_token = std::ranges::min(
+            symbol_tokens,
+            {},
+            operator_token_precedence
+        );
+
+        auto const info_matches_operator_token = [&lowest_operator_token](auto const & op_info) {
+            return op_info.identifier == lowest_operator_token.get_symbol();
+        };
+
+        // Find all operators that match lowest precedence symbol.
+        auto op_infos = std::ranges::views::filter(m_operator_registry, info_matches_operator_token);
+
+        auto const token_matches_operator_token = [&lowest_operator_token](auto const & current_token) {
+            return current_token.is_symbol() && current_token.get_symbol() == lowest_operator_token.get_symbol();
+        };
+
+        // Find last operator token matching symbol.
+        auto op_it = (
+            std::ranges::find_if(
+                a_tokens | std::views::reverse,
+                token_matches_operator_token
+            ) + 1
+        ).base();
+
+        if (op_it == a_tokens.begin()) {
+            const auto prefix_operator = std::ranges::find_if(op_infos, [](auto const & op_info) {
+                return op_info.type == operator_type::unary && op_info.association == operator_association::right;
+            });
+
+            if (prefix_operator != op_infos.end()) {
+                expression_tree->set_op(prefix_operator->mapped_operation);
+
+                expression_tree->set_operand(
+                    0,
+                    parse_expression(
+                        a_semantic_unit,
+                        { a_tokens.begin() + 1, a_tokens.end() }
+                    )
+                );
+            } else {
+                // TODO: Throw error.
+            }
+        }
+        else if (op_it == a_tokens.end() - 1) {
+            const auto postfix_operator = std::ranges::find_if(op_infos, [](auto const & op_info) {
+                return op_info.type == operator_type::unary && op_info.association == operator_association::left;
+            });
+
+            if (postfix_operator != op_infos.end()) {
+                expression_tree->set_op(postfix_operator->mapped_operation);
+
+                expression_tree->set_operand(
+                    0,
+                    parse_expression(
+                        a_semantic_unit,
+                        { a_tokens.begin(), a_tokens.end() - 1 }
+                    )
+                );
+            } else {
+                // TODO: Throw error.
+            }
+        }
+        else {
+            auto const matched_op = *op_infos.begin();
+
+            // Match first operator in series if right associated.
+            if (matched_op.association == operator_association::right) {
+                op_it = std::ranges::find_if(a_tokens, token_matches_operator_token);
+            }
+
+            if (matched_op.type == operator_type::binary) {
+                expression_tree->set_op(matched_op.mapped_operation);
+
+                expression_tree->set_operand(
+                    0,
+                    parse_expression(
+                        a_semantic_unit,
+                        { a_tokens.begin(), op_it }
+                    )
+                );
+
+                expression_tree->set_operand(
+                    1,
+                    parse_expression(
+                        a_semantic_unit,
+                        { op_it + 1, a_tokens.end() }
+                    )
+                );
+            }
+        }
+
+        return std::move(expression_tree);
+    }
+
+
+}
